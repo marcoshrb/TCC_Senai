0a51e45e0b2adeb6b71228ad264ff8d50395a04c
marcos <marcoshr_baptista@hotmail.com>
2024-07-03 10:30:10 -0300

    Course finished

--- deteccao_mao.py
+++ deteccao_mao.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
 import os
+import numpy as np
 import webbrowser
 from time import sleep
 from pynput.keyboard import Controller, Key
@@ -32,6 +33,10 @@ offset = 50
 contador = 0
 texto = '>'
 teclado = Controller()
+img_quadro = np.ones((resolucao_y, resolucao_x, 3), np.uint8) * 255
+cor_pincel = (255, 0, 0)
+espessura_pincel = 7
+x_quadro, y_quadro = 0, 0
 
 def encontra_coordenada_maos(img, lado_invertido = False):
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -141,9 +146,47 @@ while True:
             #     os.system('TASKKILL /IM chrome.exe')
             # if info_dedos_mao1 == [True, True, False, False, True]:
             #     break
-        
+    
+    if len(todas_maos) == 2:
+        info_dedos_mao1 = dedos_levantados(todas_maos[0])
+        info_dedos_mao2 = dedos_levantados(todas_maos[1])
 
+        indicador_x, indicador_y, indicador_z = todas_maos[0]['coordenadas'][8]
+
+        if sum(info_dedos_mao2) == 1:
+            cor_pincel = AZUL
+        elif sum(info_dedos_mao2) == 2:
+            cor_pincel = VERDE
+        elif sum(info_dedos_mao2) == 3:
+            cor_pincel = VERMELHO
+        elif sum(info_dedos_mao2) == 4:
+            cor_pincel = BRANCO
+        else:
+            img_quadro = np.ones((resolucao_y, resolucao_x, 3), np.uint8) * 255
+
+        if indicador_z<-40:
+            espessura_pincel = 30
+        elif indicador_z<=-30:
+            espessura_pincel = 20 
+        else:
+            espessura_pincel = 10
+        cv2.circle(img, (indicador_x, indicador_y), espessura_pincel, cor_pincel, cv2.FILLED)
+        
+        if info_dedos_mao1 == [ False, True, False, False, False]:
+            if x_quadro == 0 and y_quadro == 0:
+                x_quadro, y_quadro = indicador_x, indicador_y
+            
+            cv2.line(img_quadro, (x_quadro, y_quadro),(indicador_x, indicador_y), cor_pincel, espessura_pincel)
+
+            x_quadro, y_quadro = indicador_x, indicador_y
+        else:
+            x_quadro, y_quadro = 0, 0
+        
+        img = cv2.addWeighted(img, 1, img_quadro, 0.4, 0)
+        
+            
     cv2.imshow('imagem', img)
+    cv2.imshow('quadro', img_quadro)
     tecla = cv2.waitKey(1)
     if tecla == 27:
         break   
@@ -151,3 +194,4 @@ while True:
 with open('texto.txt', 'w') as arquivo:
     arquivo.write(texto) 
 
+cv2.imwrite('quadro.png', img_quadro)
