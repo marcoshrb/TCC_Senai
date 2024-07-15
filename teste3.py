356f2b8f94514ade5508e3bac340aef1968c2d0c
marcos <marcoshr_baptista@hotmail.com>
2024-07-15 11:14:38 -0300

    face tracking: function calculate_direction

--- teste3.py
+++ teste3.py
@@ -2,31 +2,40 @@ import cv2
 import mediapipe as mp
 import numpy as np
 import pyautogui
+import math
 
 mp_drawing = mp.solutions.drawing_utils
 mp_face_mesh = mp.solutions.face_mesh
 screen_w, screen_h = pyautogui.size()
 
-p_olho_esquerdo = [385, 380, 387, 373, 362, 263]
-p_olho_direito = [160, 144, 158, 153, 33, 133]
-p_olhos = p_olho_esquerdo + p_olho_direito
+ponto_central_idx = 9
+ponto_esquerda_idx = 356
+ponto_direita_idx = 127
 
-def calculo_ear(face, p_olho_dir, p_olho_esq):
-    try:
-        face = np.array([[coord.x, coord.y] for coord in face])
-        face_esq = face[p_olho_esq, :]
-        face_dir = face[p_olho_dir, :]
+cap = cv2.VideoCapture(0)
 
-        ear_esq = (np.linalg.norm(face_esq[0] - face_esq[1]) + np.linalg.norm(face_esq[2] - face_esq[3])) / (2 * (np.linalg.norm(face_esq[4] - face_esq[5])))
-        ear_dir = (np.linalg.norm(face_dir[0] - face_dir[1]) + np.linalg.norm(face_dir[2] - face_dir[3])) / (2 * (np.linalg.norm(face_dir[4] - face_dir[5])))
-    except:
-        ear_esq = 0.0
-        ear_dir = 0.0
-    
-    return ear_esq, ear_dir
-    
-ear_linear = 0.2
-cap = cv2.VideoCapture(1)
+cv2.namedWindow('Camera', cv2.WINDOW_NORMAL)
+cv2.setWindowProperty('Camera', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+def calculate_direction(face_landmarks, largura, comprimento):
+
+    ponto_central = face_landmarks.landmark[ponto_central_idx]
+    ponto_esquerda = face_landmarks.landmark[ponto_esquerda_idx]
+    ponto_direita = face_landmarks.landmark[ponto_direita_idx]
+
+    ponto_central_cv = mp_drawing._normalized_to_pixel_coordinates(ponto_central.x, ponto_central.y, largura, comprimento)
+    ponto_esquerda_cv = mp_drawing._normalized_to_pixel_coordinates(ponto_esquerda.x, ponto_esquerda.y, largura, comprimento)
+    ponto_direita_cv = mp_drawing._normalized_to_pixel_coordinates(ponto_direita.x, ponto_direita.y, largura, comprimento)
+
+    distancia_esquerda = math.sqrt((ponto_central_cv[0] - ponto_esquerda_cv[0])**2 + (ponto_central_cv[1] - ponto_esquerda_cv[1])**2)
+    distancia_direita = math.sqrt((ponto_central_cv[0] - ponto_direita_cv[0])**2 + (ponto_central_cv[1] - ponto_direita_cv[1])**2)
+
+    if distancia_esquerda > distancia_direita:
+        return "Direita"
+    elif distancia_direita > distancia_esquerda:
+        return "Esquerda"
+    else:
+        return "Centro"
 
 with mp_face_mesh.FaceMesh(min_detection_confidence=0.5, min_tracking_confidence=0.5, refine_landmarks=True) as facemesh:
     while cap.isOpened():
@@ -44,31 +53,30 @@ with mp_face_mesh.FaceMesh(min_detection_confidence=0.5, min_tracking_confidence
 
         try:
             for face_landmarks in saida_facemesh.multi_face_landmarks:
-                face = face_landmarks.landmark
+                for id_coord in [ponto_central_idx, ponto_esquerda_idx, ponto_direita_idx]:
+                    coord_xyz = face_landmarks.landmark[id_coord]
+                    coord_cv = mp_drawing._normalized_to_pixel_coordinates(coord_xyz.x, coord_xyz.y, largura, comprimento)
+                    cv2.circle(frame, coord_cv, 2, (255, 0, 0), -1)
 
-                for id, landmark in enumerate(face[474:478]):
-                    # mudar aq para mover o mouse uma maneira melhor
+                # for id, landmark in enumerate(face[474:478]):
+                #     # mudar aq para mover o mouse uma maneira melhor
 
-                    x = int(landmark.x * largura)
-                    y = int(landmark.y * comprimento)
-                    if id == 0 :
-                        screen_x = int(screen_w / largura * x)
-                        screen_y = int(screen_h / comprimento * y)
-                        screen_x = screen_w - screen_x
-                        pyautogui.moveTo(screen_x, screen_y)
+                #     x = int(landmark.x * largura)
+                #     y = int(landmark.y * comprimento)
+                #     if id == 0 :
+                #         screen_x = int(screen_w / largura * x)
+                #         screen_y = int(screen_h / comprimento * y)
+                #         screen_x = screen_w - screen_x
+                #         pyautogui.moveTo(screen_x, screen_y)
 
-                for id_coord, coord_xyz in enumerate(face):
-                    if id_coord in p_olhos:
-                        coord_cv = mp_drawing._normalized_to_pixel_coordinates(coord_xyz.x, coord_xyz.y, largura, comprimento)
-                        cv2.circle(frame, coord_cv, 2, (255, 0, 0), -1)
-                ear_esquerda, ear_direita = calculo_ear(face, p_olho_direito, p_olho_esquerdo)
-                
-                if ear_esquerda < ear_linear and ear_direita > ear_linear:
-                    pyautogui.click()
+                direcao = calculate_direction(face_landmarks, largura, comprimento)
+                frame = cv2.flip(frame, 1)
+                cv2.putText(frame, f"Direcao: {direcao}", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
 
         except:
             pass
-
+        
+        
         cv2.imshow('Camera', frame)
         if cv2.waitKey(10) & 0xFF == ord('c'):
             break
