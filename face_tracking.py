b0ff0685220b21c64f322cd3f4fb5354b2320151
marcos <marcoshr_baptista@hotmail.com>
2024-07-23 11:08:01 -0300

    faceTracking : new

--- face_tracking.py
+++ face_tracking.py
@@ -4,6 +4,8 @@ import numpy as np
 import pyautogui
 import math
 
+pyautogui.FAILSAFE = False
+
 mp_drawing = mp.solutions.drawing_utils
 mp_face_mesh = mp.solutions.face_mesh
 screen_w, screen_h = pyautogui.size()
@@ -11,45 +13,65 @@ screen_w, screen_h = pyautogui.size()
 ponto_central_idx = 9
 ponto_esquerda_idx = 356
 ponto_direita_idx = 127
+p_olho_esquerdo = [385, 380, 387, 373, 362, 263]
+p_olho_direito = [160, 144, 158, 153, 33, 133]
+p_olhos = p_olho_esquerdo + p_olho_direito
+
+ponto_central = 0
+ponto_esquerdo = 0
+ponto_direito = 0
+
+stabiler_size = 5
+stabiler = [(0,0)] * stabiler_size
+
+ear_linear = 0.3
 
 cap = cv2.VideoCapture(0)
 
-cv2.namedWindow('Camera', cv2.WINDOW_NORMAL)
-cv2.setWindowProperty('Camera', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+def calculo_ear(face, p_olho_dir, p_olho_esq):
+    try:
+        face = np.array([[coord.x, coord.y] for coord in face])
+        face_esq = face[p_olho_esq, :]
+        face_dir = face[p_olho_dir, :]
 
-max_x = 0
-min_x = 10000
+        ear_esq = (np.linalg.norm(face_esq[0] - face_esq[1]) + np.linalg.norm(face_esq[2] - face_esq[3])) / (2 * (np.linalg.norm(face_esq[4] - face_esq[5])))
+        ear_dir = (np.linalg.norm(face_dir[0] - face_dir[1]) + np.linalg.norm(face_dir[2] - face_dir[3])) / (2 * (np.linalg.norm(face_dir[4] - face_dir[5])))
+    except:
+        ear_esq = 0.0
+        ear_dir = 0.0
+        
+    return ear_dir, ear_esq
 
-def calculate_direction(face_landmarks, largura, comprimento):
+def center(points):
+    length = len(points)
+    dimensions = len(points[0])
+    
+    center = [0] * dimensions
 
-    ponto_central = face_landmarks.landmark[ponto_central_idx]
-    ponto_esquerda = face_landmarks.landmark[ponto_esquerda_idx]
-    ponto_direita = face_landmarks.landmark[ponto_direita_idx]
+    for point in points:
+        for i in range(dimensions):
+            center[i] += point[i]
 
-    ponto_central_cv = mp_drawing._normalized_to_pixel_coordinates(ponto_central.x, ponto_central.y, largura, comprimento)
-    ponto_esquerda_cv = mp_drawing._normalized_to_pixel_coordinates(ponto_esquerda.x, ponto_esquerda.y, largura, comprimento)
-    ponto_direita_cv = mp_drawing._normalized_to_pixel_coordinates(ponto_direita.x, ponto_direita.y, largura, comprimento)
+    return tuple([c / length for c in center]) 
 
-    distancia_esquerda = math.sqrt((ponto_central_cv[0] - ponto_esquerda_cv[0])**2 + (ponto_central_cv[1] - ponto_esquerda_cv[1])**2)
-    distancia_direita = math.sqrt((ponto_central_cv[0] - ponto_direita_cv[0])**2 + (ponto_central_cv[1] - ponto_direita_cv[1])**2)
+def direction(a, b):
+    dir = [a[i] - b[i] for i in range(len(a))]
+    length = math.sqrt(sum([d ** 2 for d in dir]))
+    if length == 0:
+        return [0] * len(dir)
+    return tuple([dir[i] / length for i in range(len(dir))])
 
-    print("distancia_direita: ", distancia_direita)
-    print("distancia_esquerda: ", distancia_esquerda)
+def distanceTo(point, direction, distance):
+    return tuple([point[i] + direction[i] * distance for i in range(len(point))])
 
-    if distancia_esquerda > distancia_direita:
-        # valor = (distancia_esquerda - distancia_direita) * (-1)
-        # valor = distancia_direita * (-1)
-        valor = distancia_esquerda * (-1)
-    elif distancia_direita > distancia_esquerda:
-        # valor = distancia_direita - distancia_esquerda
-        # valor = distancia_esquerda
-        valor = distancia_direita
-    else:
-        valor = 0
+def normalize(value, min, max):
+    return (value - min) / (max - min)
 
-    return valor
+def rescale(value, originalscale, targetscale):
+    return targetscale[0] + (normalize(value, originalscale[0], originalscale[1]) * (targetscale[1] - targetscale[0]))
 
-with mp_face_mesh.FaceMesh(min_detection_confidence=0.5, min_tracking_confidence=0.5, refine_landmarks=True) as facemesh:
+
+with mp_face_mesh.FaceMesh(max_num_faces=1, min_detection_confidence=0.5, min_tracking_confidence=0.5) as facemesh:
     while cap.isOpened():
 
         successo, frame = cap.read()
@@ -59,46 +81,30 @@ with mp_face_mesh.FaceMesh(min_detection_confidence=0.5, min_tracking_confidence
 
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         saida_facemesh = facemesh.process(frame)
-        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-        shape_y, shape_x, _ = frame.shape
 
-        try:
-            for face_landmarks in saida_facemesh.multi_face_landmarks:
+        if saida_facemesh.multi_face_landmarks:
+            face_landmarks = saida_facemesh.multi_face_landmarks[0]
+            ponto_central = face_landmarks.landmark[ponto_central_idx]
+            ponto_esquerdo = face_landmarks.landmark[ponto_esquerda_idx]
+            ponto_direito = face_landmarks.landmark[ponto_direita_idx]
+
+            centro_point = center([(ponto_direito.x, ponto_direito.y, ponto_direito.z),(ponto_esquerdo.x, ponto_esquerdo.y, ponto_esquerdo.z)])
+            direcao = direction((ponto_central.x, ponto_central.y, ponto_central.z), centro_point)
                 
-                
-                ponto_central = face_landmarks.landmark[ponto_central_idx]
-                ponto_central_cv = mp_drawing._normalized_to_pixel_coordinates(ponto_central.x, ponto_central.y, shape_x, shape_y)
+            screen_x = rescale(direcao[0], (0.3, -0.3), (0, screen_w))
+            screen_y = rescale(direcao[1], (-0.4, 0), (0, screen_h))
+            
+            stabiler.pop(0)
+            stabiler.append((screen_x, screen_y))
 
-                valor = calculate_direction(face_landmarks, shape_x, shape_y)
+            avg_x, avg_y = np.mean(stabiler, axis=0)
 
-                screen_x = int(ponto_central_cv[0] * screen_w / shape_x)
-                screen_y = int(ponto_central_cv[1] * screen_h / shape_y)
-
-                screen_x = screen_w - screen_x
-
-                if screen_x > max_x:
-                    max_x = screen_x
-
-                if screen_x < min_x:
-                    min_x = screen_x
-                
-                pyautogui.moveTo(screen_x, screen_y)
-        except:
-            pass
-        
-        centro_x = int(shape_x / 2)
-        centro_y = int(shape_y / 2)
-        cv2.circle(frame, (centro_x, centro_y), 2, (255, 0, 0), -1)
-        cv2.circle(frame, (shape_x - 3, centro_y), 2, (255, 0, 0), -1)
-        cv2.circle(frame, (5, centro_y), 2, (255, 0, 0), -1)
-
-        frame = cv2.flip(frame, 1)
-        
-        cv2.putText(frame, f"{max_x}", (30, 30), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 1)
-        cv2.putText(frame, f"{min_x}", (30, 60), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 1)
-        cv2.imshow('Camera', frame)
-        if cv2.waitKey(10) & 0xFF == ord('c'):
-            break
+            pyautogui.moveTo(avg_x, avg_y)
+            
+            ear_dir, ear_esq = calculo_ear(face_landmarks.landmark, p_olho_direito, p_olho_esquerdo)
+            
+            if (ear_dir < ear_linear and ear_esq >= ear_linear) or (ear_esq < ear_linear and ear_dir >= ear_linear):
+                pyautogui.click()
         
 cap.release()
 cv2.destroyAllWindows()
