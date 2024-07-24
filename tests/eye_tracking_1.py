778495c412428e907c7db2f15fa5343f3424d869
ilunnie <luisgustavocaris2004@gmail.com>
2024-07-24 11:13:14 -0300

    eye_tracking: eye direction

--- tests/eye_tracking_1.py
+++ tests/eye_tracking_1.py
@@ -1,25 +1,25 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-from utils import draw_line, draw_point, draw_sequence_lines
+from utils import center, direction, distance, distanceTo, draw_line, draw_point, draw_sequence_lines
 
-face_left   = (0.6694955825805664,  0.361983060836792,      0.09792782366275787)
-face_center = (0.6144697070121765,  0.33753475546836853,    -0.031630001962184906)
-face_right  = (0.492043673992157,   0.36959004402160645,    0.04289006441831589)
-left_eye_1  = (0.6406434178352356,  0.3604418635368347,     0.004836307838559151)
-left_eye_2  = (0.6391898989677429,  0.3729094862937927,     0.00774157652631402)
-left_eye_3  = (0.6527047753334045,  0.3598087728023529,     0.011264350265264511)
-left_eye_4  = (0.6509537696838379,  0.3716781735420227,     0.013929076492786407)
-left_eye_5  = (0.6289421319961548,  0.3701968789100647,     0.006444016937166452)
-left_eye_6  = (0.6580685973167419,  0.3640631437301636,     0.023414332419633865)
-right_eye_1 = (0.5559569001197815,  0.3635804057121277,     -0.019560417160391808)
-right_eye_2 = (0.5597713589668274,  0.37535330653190613,    -0.016010621562600136)
-right_eye_3 = (0.5691121220588684,  0.3621540069580078,     -0.018439121544361115)
-right_eye_4 = (0.5730513334274292,  0.3741667866706848,     -0.014864765107631683)
-right_eye_5 = (0.5467172265052795,  0.3684508800506592,     -0.012340568006038666)
-right_eye_6 = (0.5836833119392395,  0.370613157749176,      -0.009705213829874992)
-left_pupil  = (0.5620282888412476,  0.3672904968261719,     -0.01492978073656559)
-right_pupil = (0.6461473107337952,  0.36536380648612976,    0.01136911753565073)
+face_left   = (0.5789413452148438, 0.38011884689331055, 0.1471441388130188)
+face_center = (0.45492228865623474, 0.3015148937702179, -0.03611652925610542)
+face_right  = (0.28919851779937744, 0.3820362389087677, 0.10921341925859451)
+left_eye_1  = (0.5121340155601501, 0.34886354207992554, 0.006470124237239361)
+left_eye_2  = (0.5091149806976318, 0.3724062740802765, 0.009929264895617962)
+left_eye_3  = (0.5331736207008362, 0.35120952129364014, 0.013162153773009777)
+left_eye_4  = (0.5297144055366516, 0.37355637550354004, 0.01645396091043949)
+left_eye_5  = (0.4897236227989197, 0.3660852611064911, 0.01213004719465971)
+left_eye_6  = (0.5446235537528992, 0.3627241551876068, 0.030554475262761116)
+right_eye_1 = (0.3612024188041687, 0.35077184438705444, -0.006898714229464531)
+right_eye_2 = (0.36593154072761536, 0.3737955391407013, -0.0030721137300133705)
+right_eye_3 = (0.3846029043197632, 0.34944987297058105, -0.008740615099668503)
+right_eye_4 = (0.38933444023132324, 0.37200239300727844, -0.004818103276193142)
+right_eye_5 = (0.3469381034374237, 0.3628503680229187, 0.007368059828877449)
+right_eye_6 = (0.4103745222091675, 0.3665928542613983, 0.0015839249826967716)
+left_pupil  = (0.5204094052314758, 0.35966262221336365, 0.01501416601240635)
+right_pupil = (0.37390753626823425, 0.359592467546463, -0.0021094230469316244)
 
 face = [face_left, face_center, face_right]
 left_eye = [left_eye_1, left_eye_2, left_eye_3, left_eye_4, left_eye_5, left_eye_6]
@@ -30,12 +30,29 @@ ax = fig.add_subplot(111, projection='3d')
 ax.grid(False)
 
 draw_sequence_lines(ax, face)
-for point in left_eye:
-    draw_point(ax, point)
-for point in right_eye:
-    draw_point(ax, point)
+for point in left_eye + right_eye:
+    draw_point(ax, point, 'red')
 draw_point(ax, left_pupil)
-draw_point(ax, right_eye)
+draw_point(ax, right_pupil)
+
+vision_distance = 0.1
+
+left_eye_center = center(left_eye)
+left_eye_direction = direction(left_pupil, left_eye_center)
+left_vision = distanceTo(left_pupil, left_eye_direction, vision_distance)
+draw_point(ax, left_eye_center, 'blue')
+draw_line(ax, left_pupil, left_vision, 'green')
+
+right_eye_center = center(right_eye)
+right_eye_direction = direction(right_pupil, right_eye_center)
+right_vision = distanceTo(right_pupil, right_eye_direction, vision_distance)
+draw_point(ax, right_eye_center, 'blue')
+draw_line(ax, right_pupil, right_vision, 'green')
+
+eyes_center = center(left_eye + right_eye)
+vision_direction = center([left_eye_direction, right_eye_direction])
+vision = distanceTo(eyes_center, vision_direction, vision_distance)
+draw_line(ax, eyes_center, vision, 'pink')
 
 ax.set_xticklabels([])
 ax.set_yticklabels([])
---
+++ tests/eye_tracking_2.py
@@ -0,0 +1,57 @@
+import cv2
+import mediapipe as mp
+import pyautogui
+
+mp_drawing = mp.solutions.drawing_utils
+mp_face_mesh = mp.solutions.face_mesh
+screen_w, screen_h = pyautogui.size()
+
+cap = cv2.VideoCapture(0)
+
+##### INDEXES #####
+face_idx = [356, 9, 127]
+
+left_eye_idx = [385, 380, 387, 373, 362, 263]
+rigth_eye_idx = [160, 144, 158, 153, 33, 133]
+
+pupila_direito = [468]
+pupila_esquerda = [473]
+###################
+
+cv2.namedWindow('Camera', cv2.WINDOW_NORMAL)
+
+with mp_face_mesh.FaceMesh(max_num_faces=1, min_detection_confidence=0.5, min_tracking_confidence=0.5, refine_landmarks=True) as facemesh:
+    while cap.isOpened():
+        successo, frame = cap.read()
+        
+        if not successo:
+            continue
+        
+        saida_facemesh = facemesh.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+        print("=================")
+        
+        if saida_facemesh.multi_face_landmarks:
+            shape_y, shape_x, _ = frame.shape
+            face_landmarks = saida_facemesh.multi_face_landmarks[0]
+            
+            for idx in face_idx:
+                point = face_landmarks.landmark[idx]
+                coord = mp_drawing._normalized_to_pixel_coordinates(point.x, point.y, shape_x, shape_y)
+                cv2.circle(frame, coord, 2, (255, 0, 0), -1)
+                print(f"{idx} = [{point.x}, {point.y}, {point.z}]")
+                
+            for idx in left_eye_idx + rigth_eye_idx:
+                point = face_landmarks.landmark[idx]
+                coord = mp_drawing._normalized_to_pixel_coordinates(point.x, point.y, shape_x, shape_y)
+                cv2.circle(frame, coord, 1, (0, 255, 0), -1)
+                print(f"{idx} = [{point.x}, {point.y}, {point.z}]")
+                
+            for idx in pupila_esquerda + pupila_direito:
+                point = face_landmarks.landmark[idx]
+                coord = mp_drawing._normalized_to_pixel_coordinates(point.x, point.y, shape_x, shape_y)
+                cv2.circle(frame, coord, 5, (0, 0, 255), 2)
+                print(f"{idx} = [{point.x}, {point.y}, {point.z}]")
+            
+        cv2.imshow('Camera', frame)
+        if cv2.waitKey(10) & 0xFF == 27:
+            break
